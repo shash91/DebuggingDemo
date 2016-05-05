76a7679941a35bcfd6d73b6438a105ab29766e5b
shash91 <shashika@davton.com>
2016-05-05 11:22:55 +0530

    Debugging MVC Applications
    Demonstrates basic debugging functions using basic MVC template

---
+++ DebuggingDemo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DebuggingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int firstVal = 10;
+            int secondVal = 0;
+            int result = firstVal / secondVal;
+           // ViewBag.Message = "Welcome to ASP.Net MVC";
+            return View(result);
+        }
+    }
+}
